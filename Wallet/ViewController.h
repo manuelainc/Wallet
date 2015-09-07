61de40c08fccd57771edab01796d8cbda4739d1f
Manuel Amado <manuelainc@gmail.com>
2015-09-07 20:30:40 +0200

    Initial Commit

---
+++ Wallet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wallet
+//
+//  Created by Sherpa on 7/9/15.
+//  Copyright (c) 2015 manuelainc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
